47f096167b5879860389b1a8a9807764fccc0438
Raju kushwaha <143260584+rajukushwaha210361@users.noreply.github.com>
2025-05-03 11:57:07 +0530

    Update utils.js
    backend url connected

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,6 +2,6 @@ import { toast } from "react-toastify"
 export const notify=(message,type)=>{
  toast[type](message);
 }
-export const API_URL ="http://localhost:8080"
+export const API_URL ="https://task-manager-1-qrr9.onrender.com"
 
 
